33d4c7dd1275a2e96a56f433c49d959a006ca104
Nathaniel Fruni <fruninathan@gmail.com>
2025-02-07 00:20:55 +0100

    Initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="grid h-screen place-items-center h-screen">
+        <h1 class="font-mono antialiased text-6xl text-center text-neutral-200">Task App</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+<body class="h-screen bg-gradient-to-b from-purple-900 to-blue-950">
+@inertia
+</body>
+</html>
