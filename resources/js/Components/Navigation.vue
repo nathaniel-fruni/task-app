539f84b1974d2a95f842b1571470178752af2d9e
Nathaniel Fruni <fruninathan@gmail.com>
2025-02-12 14:17:48 +0100

    navigation component separation

---
+++ resources/js/Components/Navigation.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import NavLink from '../Components/NavLink.vue'
+import {ref} from 'vue'
+
+const isMenuOpen = ref<boolean>(false)
+</script>
+
+<template>
+    <nav class="flex items-center justify-between">
+
+        <div class="text-lg lg:text-2xl uppercase">
+            <Link href="/">Task App</Link>
+        </div>
+
+        <button @click="isMenuOpen = !isMenuOpen" class="md:hidden">
+            <i v-if="!isMenuOpen" class="fa-solid fa-bars"></i>
+        </button>
+
+        <div class="hidden md:flex items-center gap-20 text-lg">
+            <NavLink href="/" component="Home" name="Domov"/>
+            <NavLink href="/tasks" component="Tasks" name="Úlohy"/>
+        </div>
+
+        <div v-if="isMenuOpen"
+             class="md:hidden flex flex-col items-center gap-5 text-md md:text-lg">
+            <NavLink href="/" component="Home" name="Domov" @click="isMenuOpen = false"/>
+            <NavLink href="/tasks" component="Tasks" name="Úlohy" @click="isMenuOpen = false"/>
+        </div>
+    </nav>
+</template>
--- resources/js/Layout/Layout.vue
+++ resources/js/Layout/Layout.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import NavLink from '../Components/NavLink.vue'
-import {ref} from 'vue'
-
-const isMenuOpen = ref<boolean>(false)
+import Navigation from '../Components/Navigation.vue';
 </script>
 
 <template>
@@ -12,27 +9,7 @@ const isMenuOpen = ref<boolean>(false)
     font-poppins font-semibold"
     >
         <header class="flex-0 text-white">
-            <nav class="flex items-center justify-between">
-
-                <div class="text-lg lg:text-2xl uppercase">
-                    <Link href="/">Task App</Link>
-                </div>
-
-                <button @click="isMenuOpen = !isMenuOpen" class="md:hidden">
-                    <i v-if="!isMenuOpen" class="fa-solid fa-bars"></i>
-                </button>
-
-                <div class="hidden md:flex items-center gap-20 text-lg">
-                    <NavLink href="/" component="Home" name="Domov"/>
-                    <NavLink href="/tasks" component="Tasks" name="Úlohy"/>
-                </div>
-
-                <div v-if="isMenuOpen"
-                     class="md:hidden flex flex-col items-center gap-5 text-md md:text-lg">
-                    <NavLink href="/" component="Home" name="Domov" @click="isMenuOpen = false"/>
-                    <NavLink href="/tasks" component="Tasks" name="Úlohy" @click="isMenuOpen = false"/>
-                </div>
-            </nav>
+            <Navigation/>
         </header>
 
         <main class="flex-2">
